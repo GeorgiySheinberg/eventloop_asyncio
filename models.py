2cfcaf152472d861ff43c30b3e8729f62d3d616f
Георгий Шейнберг <georgiysheinberg@gmail.com>
2024-07-09 16:36:26 +0300

    first commit

---
+++ models.py
@@ -0,0 +1,46 @@
+import os
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncAttrs
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String
+
+POSTGRES_USER = os.getenv("POSTGRES_USER", "user")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "1234")
+POSTGRES_DB = os.getenv("POSTGRES_DB", "swapi_db")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST", "127.0.0.1")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT", '5431')
+
+PG_DSN = f'postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}'
+
+engine = create_async_engine(PG_DSN)
+
+Session = async_sessionmaker(bind=engine, expire_on_commit=False)
+
+
+class Base(DeclarativeBase, AsyncAttrs):
+    pass
+
+
+class SwapiPeople(Base):
+    __tablename__ = 'people'
+    keys = ['name', 'height', 'mass', 'hair_color', 'skin_color', 'eye_color', 'birth_year', 'gender',
+            'homeworld', 'films', 'species', 'vehicles', 'starships', 'created', 'edited', 'url']
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String, nullable=True)
+    birth_year: Mapped[str] = mapped_column(String, nullable=True)
+    height: Mapped[str] = mapped_column(String, nullable=True)
+    mass: Mapped[str] = mapped_column(String, nullable=True)
+    hair_color: Mapped[str] = mapped_column(String, nullable=True)
+    skin_color: Mapped[str] = mapped_column(String, nullable=True)
+    eye_color: Mapped[str] = mapped_column(String, nullable=True)
+    gender: Mapped[str] = mapped_column(String, nullable=True)
+    homeworld: Mapped[str] = mapped_column(String, nullable=True)
+    films: Mapped[str] = mapped_column(String, nullable=True)
+    species: Mapped[str] = mapped_column(String, nullable=True)
+    vehicles: Mapped[str] = mapped_column(String, nullable=True)
+    starships: Mapped[str] = mapped_column(String, nullable=True)
+
+
+async def init_orm():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
